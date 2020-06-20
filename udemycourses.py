958bca7e672cefbb6ce40f7834958e0e3854a354
Thom Su <tzulungs@gmail.com>
2020-06-20 17:56:35 -0400

    added comments and decomposed long functions

--- udemycourses.py
+++ udemycourses.py
@@ -23,12 +23,13 @@ def scraper(search_term='python machine learning', filter_category='Machine Lear
     previous_links=[]
 
     """
-
+# ===========================Preprocessing=======================================
     cols = ['course_link', 'customer_name', 'time_posted', 'review', 'ratings']
-    df = pd.DataFrame(columns=cols)
-    courses = list()
-    visited_links = list()
+    df = pd.DataFrame(columns=cols)  # empty DataFrame for reviews
+    courses = list()  # empty list for course details
+    visited_links = list()  # to keep tab on links that have been scraped or checked to not meet the requirement
 
+    # validate input data types
     if not isinstance(search_term, str):
         raise TypeError(
             f"Input variable, search_term, should be a string and not type {type(search_term)}.")
@@ -39,8 +40,8 @@ def scraper(search_term='python machine learning', filter_category='Machine Lear
         raise TypeError(
             "Input variable, previous_links, should be a list of previous visited links.")
 
-    visited_links.extend(previous_links)
-
+    visited_links.extend(previous_links)  # merge user given links into one list
+# ===========================Start Searching=====================================
     browser = webdriver.Chrome()
     browser.get('http://www.udemy.com')
 
@@ -51,6 +52,7 @@ def scraper(search_term='python machine learning', filter_category='Machine Lear
 
     elem.send_keys(search_term + Keys.RETURN)
 
+    # check for issues that the search yield no results.
     try:
         WebDriverWait(browser, 5).until(EC.visibility_of_any_elements_located(
             (By.XPATH, "//div[@class='panel--content-wrapper--1yFBX']//fieldset[@name='Topic']")))
@@ -58,16 +60,13 @@ def scraper(search_term='python machine learning', filter_category='Machine Lear
         print("Encounter a problem retrieving search results. Please check if search_term and filter_category are valid.")
 
     add_filter(browser, filter_category)
-
+# =================First Filtered Search Results Page Returned===================
     time.sleep(2)
+    # access links of search results
     courses_links = browser.find_elements_by_class_name("udlite-custom-focus-visible")
     links = [c.get_attribute("href") for c in courses_links]
-    try:
-        nextpage = browser.find_element_by_xpath(
-            "//div[@class='pagination--container--2wc6Z']//a[@data-page='+1']").get_attribute("href")
-    except NoSuchElementException:
-        nextpage = None
-
+    nextpage = get_nextpage(browser)  # access link to next page of search results
+# =======================Iteratively Scrape Links================================
     page_count = 1
     df, courses = scrape_links_navigator(browser, df, cols, courses, links, visited_links)
     print(
@@ -91,43 +90,54 @@ def add_filter(browser, filter_category):
     It also adds a filter to limit the language of the courses to English.
 
     """
-
-    try:
-        expandlist = browser.find_element_by_xpath(
-            "//label[contains(text(),'Topic')]/following-sibling::node()//label[@role='button']")
-        expandlist.click()
-    except NoSuchElementException:
-        pass
-    except ElementClickInterceptedException:
-        browser.execute_script("arguments[0].click();", expandlist)
-
-    filterlist = browser.find_elements_by_xpath(
+    # attempt to expand the Topic menu list
+    while True:
+        try:
+            expandlist = browser.find_element_by_xpath(
+                "//label[contains(text(),'Topic')]/following-sibling::node()//label[@role='button']")
+            expandlist.click()
+            break
+        except NoSuchElementException:
+            expose_filter_menu(browser, 'Topic')
+        except ElementClickInterceptedException:
+            browser.execute_script("arguments[0].click();", expandlist)
+            break
+    # find the checkbox that match the filter category and attempt to mark it
+    filtercat = browser.find_elements_by_xpath(
         "//div[@class='panel--content-wrapper--1yFBX']//fieldset[@name='Topic']//span[@class='filter--count--33UW8']/parent::node()")
-    ind = [t.text.split("(")[0] for t in filterlist].index(filter_category)
+    indc = [cat.text.split("(")[0] for cat in filtercat].index(filter_category)
     checkbox = browser.find_elements_by_xpath(
-        "//div[@class='panel--content-wrapper--1yFBX']//fieldset[@name='Topic']//input")[ind]
+        "//div[@class='panel--content-wrapper--1yFBX']//fieldset[@name='Topic']//input")[indc]
     try:
         checkbox.click()
     except ElementClickInterceptedException:
         browser.execute_script("arguments[0].click();", checkbox)
 
-    expandfilter = browser.find_elements_by_xpath(
-        "//label[contains(@class,'js-panel-toggler  panel--label--qoWJs') and @aria-expanded='false']")
-    ind = [exf.text for exf in expandfilter].index('Language')
-    try:
-        expandfilter[ind].click()
-    except ElementClickInterceptedException:
-        browser.execute_script("arguments[0].click();", expandfilter[ind])
-
-    filterlist = browser.find_elements_by_xpath(
+    expose_filter_menu(browser, 'Language')
+    # attempt to mark the English language checkbox
+    filterlang = browser.find_elements_by_xpath(
         "//div[@class='panel--content-wrapper--1yFBX']//fieldset[@name='Language']//span[@class='filter--count--33UW8']/parent::node()")
-    ind = [t.text.split("(")[0] for t in filterlist].index('English')
+    indl = [lang.text.split("(")[0] for lang in filterlang].index('English')
     checkbox = browser.find_elements_by_xpath(
-        "//div[@class='panel--content-wrapper--1yFBX']//fieldset[@name='Language']//input")[ind]
+        "//div[@class='panel--content-wrapper--1yFBX']//fieldset[@name='Language']//input")[indl]
     try:
         checkbox.click()
     except ElementClickInterceptedException:
         browser.execute_script("arguments[0].click();", checkbox)
+
+
+def get_nextpage(browser):
+    """
+
+    returns the link to the next page of search results if any is found
+
+    """
+
+    try:
+        return browser.find_element_by_xpath(
+            "//div[@class='pagination--container--2wc6Z']//a[@data-page='+1']").get_attribute("href")
+    except NoSuchElementException:
+        return None
 
 
 def scrape_links_navigator(browser, df, cols, courses, links, visited_links):
@@ -137,13 +147,14 @@ def scrape_links_navigator(browser, df, cols, courses, links, visited_links):
 
     """
 
-    for lk in links:
-        if lk not in visited_links:
-            browser.get(lk)
-            proceed, courses = course_scraper(browser, courses, lk)
+    for link in links:
+        if link not in visited_links:  # scrape links only if not in list
+            browser.get(link)
+            proceed, courses = course_scraper(browser, courses, link)
+            # if requirements are not met in course scraper, the link is not to be scraped.
             if proceed:
-                df = review_scraper(browser, df, cols, lk)
-            visited_links.append(lk)
+                df = review_scraper(browser, df, cols, link)
+            visited_links.append(link)
 
     return df, courses
 
@@ -157,26 +168,17 @@ def listings_page_iterator(browser, nextpage, page_count, df, cols, courses, lin
 
     browser.get(nextpage)
     page_count += 1
-
+    # check for new search results and next search result link.
     try:
         WebDriverWait(browser, 5).until(EC.visibility_of_any_elements_located(
             (By.CLASS_NAME, "udlite-custom-focus-visible")))
     except TimeoutException:
-        try:
-            nextpage = browser.find_element_by_xpath(
-                "//div[@class='pagination--container--2wc6Z']//a[@data-page='+1']").get_attribute("href")
-        except NoSuchElementException:
-            nextpage = None
-            return df, courses
+        nextpage = get_nextpage(browser)
+        return df, courses  # end scraper if no new search results and no next search result link
 
     courses_links = browser.find_elements_by_class_name("udlite-custom-focus-visible")
     links = [c.get_attribute("href") for c in courses_links]
-
-    try:
-        nextpage = browser.find_element_by_xpath(
-            "//div[@class='pagination--container--2wc6Z']//a[@data-page='+1']").get_attribute("href")
-    except NoSuchElementException:
-        nextpage = None
+    nextpage = get_nextpage(browser)
 
     df, courses = scrape_links_navigator(browser, df, cols, courses, links, visited_links)
 
@@ -191,7 +193,7 @@ def course_scraper(browser, courses, link):
     """
 
     course = dict()
-
+# =========================Minimum Requirement Check=============================
     WebDriverWait(browser, 5).until(EC.presence_of_element_located(
         (By.XPATH, "//div[contains(@data-content-group,'Landing Page')]//div[@data-purpose='enrollment']")))
     enrolled = browser.find_element_by_xpath(
@@ -203,35 +205,20 @@ def course_scraper(browser, courses, link):
         return False, courses
     language = browser.find_element_by_xpath(
         "//div[contains(@data-content-group,'Landing Page')]//div[@class='clp-lead__locale']").text
+    # check if students enrolled, number of reviews and language requirements are met
     if int(enrolled.replace(",", "")) < 500 or int(num_of_reviews.replace(",", "")) < 50 or language != "English":
-        return False, courses
-
+        return False, courses  # skip scraping if not
+# ===============================Scrape Page=====================================
     course['link'] = link
     course['title'] = browser.find_element_by_xpath("//h1").text
-
-    try:
-        browser.find_element_by_xpath(
-            "//div[@class='what-you-get']//button[contains(@class,'js-simple-collapse-more-btn')]").click()
-    except NoSuchElementException:
-        pass
-    except ElementClickInterceptedException:
-        see_more = browser.find_element_by_class_xpath(
-            "//div[@class='what-you-get']//button[contains(@class,'js-simple-collapse-more-btn')]")
-        browser.execute_script("arguments[0].click();", see_more)
-
+    # expand topic section if it is possible and capture info
+    expand_section(
+        browser, "//div[@class='what-you-get']//button[contains(@class,'js-simple-collapse-more-btn')]")
     topics = browser.find_elements_by_class_name("what-you-get__text")
     course['topics'] = ', '.join([t.text for t in topics])
-
-    try:
-        browser.find_element_by_xpath(
-            "//div[contains(@data-purpose,'course-description')]//button[contains(@class,js-simple-collapse-more-btn)]").click()
-    except NoSuchElementException:
-        pass
-    except ElementClickInterceptedException:
-        see_more = browser.find_element_by_xpath(
-            "//div[contains(@data-purpose,'course-description')]//button[contains(@class,js-simple-collapse-more-btn)]")
-        browser.execute_script("arguments[0].click();", see_more)
-
+    # expand course description section if it is possible and capture info
+    expand_section(
+        browser, "//div[contains(@data-purpose,'course-description')]//button[contains(@class,js-simple-collapse-more-btn)]")
     summary = browser.find_elements_by_xpath(
         "//div[@class='description__title']/following-sibling::*//*")
     course['summary'] = '\n'.join([s.text for s in summary[:-3]])
@@ -239,29 +226,15 @@ def course_scraper(browser, courses, link):
     course['number_of_lectures'] = browser.find_element_by_xpath("//span[@class='dib']").text
     course['total_video_duration'] = browser.find_element_by_xpath(
         "//span[@class='curriculum-header-length']").text
-
-    try:
-        toggle = WebDriverWait(browser, 5).until(EC.visibility_of_element_located(
-            (By.XPATH, '//a[@data-purpose="load-full-curriculum"]')))
-        toggle.click()
-    except TimeoutException:
-        pass
-    except ElementClickInterceptedException:
-        browser.execute_script("arguments[0].click();", toggle)
-
-    try:
-        toggle = WebDriverWait(browser, 5).until(
-            EC.visibility_of_element_located((By.XPATH, '//a[@class="sections-toggle"]')))
-        toggle.click()
-    except TimeoutException:
-        pass
-    except ElementClickInterceptedException:
-        browser.execute_script("arguments[0].click();", toggle)
-
+    # expand course lectures if it is possible
+    expand_toggle(
+        browser, '//a[@data-purpose="load-full-curriculum" or @data-purpose="toggle-section"]')
+    expand_toggle(browser, '//a[@class="sections-toggle"]')
     titles = browser.find_elements_by_xpath(
         "//div[@data-purpose='course-curriculum']//div[@class='title']")
     duration = browser.find_elements_by_xpath(
         "//div[@data-purpose='course-curriculum']//div[@class='details']")
+    # attempt to capture lecture titles and durations; if encountered a change of the DOM, wait 2 seconds
     while True:
         try:
             course['lectures_breakdown'] = list(
@@ -273,50 +246,24 @@ def course_scraper(browser, courses, link):
 
     course['original_price'] = browser.find_element_by_xpath(
         "//div[@data-purpose='course-old-price-text']//s/span").text
-
+    # find out if there is one or more instructors information and process them separately
     instructors = browser.find_elements_by_class_name("instructor--instructor--2qudS")
     if len(instructors) > 1:
-
+        # add '-&-' between the names of instructors
         course['instructor_name'] = ' -&- '.join([i.find_element_by_class_name(
             "instructor--title__link--1NJ6S").text for i in instructors])
-
-        see_more = browser.find_elements_by_class_name(
-            "instructor--view-more-wrapper__button--2egB6")
-        for s in see_more:
-            try:
-                s.click()
-            except ElementClickInterceptedException:
-                browser.execute_script("arguments[0].click();", s)
-
-        bio = browser.find_elements_by_xpath(
-            "//div[@class='instructor--instructor--2qudS']//div[@data-purpose='safely-set-inner-html:trusted-html:content']//p")
-        course['instructor_bio'] = '\n'.join([b.text.strip() for b in bio])
-
-        stats = browser.find_elements_by_xpath(
-            "//span[@class='instructor--instructor__stat-value--2Kwe1']")
-        stats = [s.text for s in stats]
+        course['instructor_bio'], stats = get_bio_stats(browser)
+        # create a list of different instructors for each stat
         course['group_instructor_rating'] = stats[::4]
         course['group_reviews'] = stats[1::4]
         course['group_students'] = stats[2::4]
         course['group_courses'] = stats[3::4]
 
     else:
-        instructor = browser.find_element_by_class_name("instructor--instructor--2qudS")
-        course['instructor_name'] = instructor.find_element_by_class_name(
+        course['instructor_name'] = instructors[0].find_element_by_class_name(
             "instructor--title__link--1NJ6S").text
-
-        stats = instructor.find_elements_by_xpath(
-            "//span[@class='instructor--instructor__stat-value--2Kwe1']")
-        stats = [s.text for s in stats]
+        course['instructor_bio'], stats = get_bio_stats(browser)
         course['instructor_rating'], course['total_reviews'], course['total_students'], course['total_courses'] = stats[0], stats[1], stats[2], stats[3]
-
-        see_more = browser.find_elements_by_class_name(
-            "instructor--view-more-wrapper__button--2egB6")
-        for s in see_more:
-            s.click()
-        bio = instructor.find_elements_by_xpath(
-            "//div[@data-purpose='safely-set-inner-html:trusted-html:content']//p")
-        course['instructor_bio'] = '\n'.join([b.text.strip() for b in bio])
 
     courses.append(course)
     return True, courses
@@ -328,8 +275,8 @@ def review_scraper(browser, df, cols, link):
     This scrapes all details about the reviews of a course.
 
     """
-
-    repeats = 10
+    # repeat the specified number of times to expand the review section
+    repeats = 5
     while repeats:
         try:
             browser.find_element_by_xpath(
@@ -339,7 +286,7 @@ def review_scraper(browser, df, cols, link):
         except StaleElementReferenceException:
             break
         repeats -= 1
-
+    # expand to reveal the complete review of long reviews that are partially hidden
     see_more = browser.find_elements_by_xpath(
         "//div[@data-purpose='landing-page-review-list']//button[contains(@class,'view-more-container--view-more__collapse-btn--1bVN9 btn btn-link')]")
     if see_more:
@@ -367,3 +314,78 @@ def review_scraper(browser, df, cols, link):
     time_posted = [w.text for w in posted_when]
 
     return pd.concat([df, pd.DataFrame(list(zip(course_link, customer_name, time_posted, review, ratings)), columns=cols)], ignore_index=True)
+
+
+def expand_section(browser, path):
+    """
+
+    attempts to expand a section and at the same time handles exceptions thrown
+
+    """
+
+    try:
+        browser.find_element_by_xpath(path).click()
+    except NoSuchElementException:
+        pass
+    except ElementClickInterceptedException:
+        see_more_section = browser.find_element_by_class_xpath(path)
+        browser.execute_script("arguments[0].click();", see_more_section)
+
+
+def expand_toggle(browser, path):
+    """
+
+    wait for element to be visible before expand a toggle; also handles exceptions thrown
+
+    """
+
+    try:
+        toggle = WebDriverWait(browser, 5).until(
+            EC.visibility_of_element_located((By.XPATH, path)))
+        toggle.click()
+    except TimeoutException:
+        pass
+    except ElementClickInterceptedException:
+        browser.execute_script("arguments[0].click();", toggle)
+
+
+def get_bio_stats(browser):
+    """
+
+    locate and capture instructor bio and stats information
+
+    """
+
+    see_more_bio = browser.find_elements_by_class_name(
+        "instructor--view-more-wrapper__button--2egB6")
+    for s in see_more_bio:
+        try:
+            s.click()
+        except ElementClickInterceptedException:
+            browser.execute_script("arguments[0].click();", s)
+
+    bio = browser.find_elements_by_xpath(
+        "//div[@class='instructor--instructor--2qudS']//div[@data-purpose='safely-set-inner-html:trusted-html:content']//p")
+    instructor_bio = '\n'.join([b.text.strip() for b in bio])
+
+    stats = browser.find_elements_by_xpath(
+        "//span[@class='instructor--instructor__stat-value--2Kwe1']")
+    stats = [s.text for s in stats]
+
+    return instructor_bio, stats
+
+
+def expose_filter_menu(browser, filter_by):
+    """
+
+    find the text of the label that matches with what is provided and attempt to expose the menu
+
+    """
+
+    exposemenu = browser.find_elements_by_xpath(
+        "//label[contains(@class,'js-panel-toggler  panel--label--qoWJs') and @aria-expanded='false']")
+    ind = [fltr.text for fltr in exposemenu].index(filter_by)
+    try:
+        exposemenu[ind].click()
+    except ElementClickInterceptedException:
+        browser.execute_script("arguments[0].click();", exposemenu[ind])
